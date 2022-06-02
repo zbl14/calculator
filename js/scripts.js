17cc057b27ff43aebb8bf600f3a96335031d96d5
zbl14 <ifthereisoneday@gmail.com>
2022-06-02 08:08:45 -0700

    add remainder function

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,11 @@ function divide(number1, number2) {
   return number1 / number2;
 }
 
+function remainder(number1, number2) {
+  return number1 % number2;
+}
+
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
-const result = multiply(number1, number2);
+const result = add(number1, number2);
 alert(result);
